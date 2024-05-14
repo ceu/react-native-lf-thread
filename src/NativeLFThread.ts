16d86005ddfd619f7ecad74cb6c6507b2e63b700
Bitcoin <lifan3285164@gmail.com>
2024-05-14 14:32:42 +0800

    docs(README.md): update README.md

--- src/NativeLFThread.ts
+++ src/NativeLFThread.ts
@@ -4,7 +4,7 @@ import { TurboModuleRegistry } from 'react-native';
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
 
-  asyncFn: (fn: Function, onFinshed: (result: any) => void) => void;
+  asyncFn: (fn: () => void, onFinshed: (result: any) => void) => void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('LFThread');
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ export function multiply(a: number, b: number): number {
   return LFThread.multiply(a, b);
 }
 
-export function asyncFn(fn: Function, onFinshed: (result: any) => void): void {
+export function asyncFn(
+  fn: () => void,
+  onFinshed: (result: any) => void
+): void {
   return LFThread.asyncFn(fn, onFinshed);
 }
